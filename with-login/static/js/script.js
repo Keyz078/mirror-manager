750269a0999e33e655da7df66d4f607fa4f18932
keyz078 <luqinthar@gmail.com>
2025-03-07 07:23:44 +0700

    fix prevent default while deleting container

--- with-login/static/js/script.js
+++ with-login/static/js/script.js
@@ -47,6 +47,7 @@ function refreshTable() {
             document.querySelectorAll('.delete-btn').forEach(button => {
                 button.addEventListener('click', function(event) {
                     if (!confirm('Are you sure you want to delete this container?')) {
+                        event.preventDefault();
                     }
                 });
             });
